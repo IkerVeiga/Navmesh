2c3a5063917bad007da0b3c27f9840183640c250
IkerVeiga <veiga.iker@opendeusto.es>
2024-05-16 09:07:23 +0200

    off-mesh link

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Obstacle : MonoBehaviour
 {
+    private NavMeshAgent agent;
+    private int currentGoal;
+
+    public Transform[] goals;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (agent.remainingDistance - agent.stoppingDistance >= 0 && agent.remainingDistance - agent.stoppingDistance <= 0.2 && !agent.pathPending)
+        {
+            currentGoal++;
+        }
+        if (currentGoal == goals.Length)
+        {
+            currentGoal = 0;
+        }
+        agent.SetDestination(this.goals[currentGoal].position);
     }
 }
